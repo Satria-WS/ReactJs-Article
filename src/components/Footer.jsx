1221dff20eb5ca4d278a6db927f0d7b99f713e70
Satria-WS <extencezone02@gmail.com>
2022-07-12 18:47:02 +0700

    smoking

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Test from "./test";
+import symbolS from "../assets/images/S.png";
 
 const Footer = () => {
   return (
     <>
       <div className="globalBox">
         <div className="boxFooter">
-            <div className="footerBootstrap">FooterBootrap</div>
+          <img className="Symbol_S" src={symbolS} alt="logo" />
+          <div className="serbaSerbi Horizontal">SerbaSerbi</div>
+          <div className="footerBootstrap">
+            <Test />
+          </div>
         </div>
       </div>
     </>
