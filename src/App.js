5f8b25b0a667e8ca81479737441ce6369f99a766
Satria-WS <extencezone02@gmail.com>
2022-07-12 12:07:40 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./assets/css/header.css";
+import "./assets/css/footer.css";
+import "./assets/css/globalArticle.css";
+
+import TestCode from "./components/test";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App globalWord">
+      <Header />
+      {/* <TestCode/> */}
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header() {
+  return (
+    <>
+      
+      <div className="globalBox"></div>
+    </>
+  );
+}
+
+export default Header;
