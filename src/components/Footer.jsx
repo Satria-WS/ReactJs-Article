e1b1dae196b143b1962a601427aacd6335987b94
Satria-WS <extencezone02@gmail.com>
2022-07-12 21:26:25 +0700

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,39 @@ const Footer = () => {
     <>
       <div className="globalBox">
         <div className="boxFooter">
-          <img style={{top:"74px"}} className="Symbol_S" src={symbolS} alt="logo" />
-          <div style={{top:"88px"}} className="serbaSerbi Horizontal">SerbaSerbi</div>
-          <div style={{top:"75px"}} className="menuBantuan">Menu</div>
-          <div className="menuBantuan">Bantuan</div>
-          <div className="homePusatPrivacy">Home</div>
-          <div className="homePusatPrivacy">Pusat Bantuan</div>
-          <div className="homePusatPrivacy">Privacy Policy</div>
+          <img
+            style={{ top: "74px" }}
+            className="Symbol_S"
+            src={symbolS}
+            alt="logo"
+          />
+          <div style={{ top: "88px" }} className="serbaSerbi Horizontal">
+            SerbaSerbi
+          </div>
+          <div style={{ top: "75px", left: "429px" }} className="menuBantuan">
+            Menu
+          </div>
+          <div style={{ top: "75px", left: "563px" }} className="menuBantuan">
+            Bantuan
+          </div>
+          <div
+            style={{ top: "121px", left: "395px" }}
+            className="homePusatPrivacy"
+          >
+            Home
+          </div>
+          <div
+            style={{ top: "121px", left: "553px" }}
+            className="homePusatPrivacy"
+          >
+            Pusat Bantuan
+          </div>
+          <div
+            style={{ top: "154px", left: "553px" }}
+            className="homePusatPrivacy"
+          >
+            Privacy Policy
+          </div>
           <div className="footerBootstrap">
             <Test />
           </div>
--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -4,9 +4,39 @@ import ReactDom from "react-dom";
 const Middle = () => {
   return (
     <>
+      {/* backend */}
+
+      {/* kolom-1 */}
       <div className="globalBox">
-        <div className="MiddleColoumn_1"> coloumn 1</div>
-        <div className="MiddleColoumn_2">coloumn 2</div>
+        <div className="MiddleColoumn_1">
+          <div className="boxColoumn_1">
+            image
+            <img />
+          </div>
+          <div className="boxColoumn_1">
+            image
+            <img />
+          </div>
+          <div className="boxColoumn_1">
+            image
+            <img />
+          </div>
+        </div>
+
+        {/* kolom-2 */}
+        <div className="MiddleColoumn_2">
+          <div className="boxColoumn_2">2</div>
+        </div>
+      </div>
+
+      {/* page number */}
+      <div className="globalBox">
+        <div className="prevPage">Prev</div>
+        <div className="nextPage">Next</div>
+        <div className="borderPageOne">
+          <div className="pageOne">1</div>
+        </div>
+        <div className="pageTwo">2</div>
       </div>
     </>
   );
