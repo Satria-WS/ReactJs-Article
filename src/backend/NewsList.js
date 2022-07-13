0c2d64cc751bb980a6076794c536e256f093b886
Satria-WS <extencezone02@gmail.com>
2022-07-13 18:23:38 +0700

    fetch API content

--- src/backend/NewsList.js
+++ src/backend/NewsList.js
@@ -8,27 +8,49 @@ const NewsList = () => {
   useEffect(() => {
     const getArticles = async () => {
       const res = await Axios.get(
-        "https://newsapi.org/v2/everything?q=tesla&from=2022-06-13&sortBy=publishedAt&apiKey=af178ec04288441788f0780ea7b0f93e"
+        "https://newsapi.org/v2/everything?apiKey=af178ec04288441788f0780ea7b0f93e&q=tesla&from=2022-07-01&sortBy=popularity&language=id&pageSize=1&page=1"
       );
-      console.log(res);
-      //console.log(res.data.articles);
+      //console.log(res);
+    //   console.log(res.data.articles);
+      //console.log(res.data.articles[0].author); //Misrohatun Hasanah
+
       setArticles(res.data.articles);
     };
     getArticles();
-  },[]);
+  }, []);
   return (
-    <div>
-      {articles.map(({ title, description, url, urlToImage }) => (
-        <NewsItem
-          title={title}
-          description={description}
-          url={url}
-          urlToImage={urlToImage}
-        />
-      ))}
+    <>
+      {/* article original */}
+      {/*       <div>
+        {articles.map(
+          ({ title, description, url, urlToImage, publishedAt }) => (
+            <NewsItem
+              title={title}
+              description={description}
+              url={url}
+              urlToImage={urlToImage}
+              publishedAt={publishedAt}
+            />
+          )
+        )}
+      </div> */}
 
-      {/* <NewsItem/> */}
-    </div>
+      {/* article page 2 */}
+      <div>
+        {articles.map(
+          ({ title, url, urlToImage, publishedAt, content, description }) => (
+            <NewsItem
+              title={title}
+              url={url}
+              urlToImage={urlToImage}
+              publishedAt={publishedAt}
+              content={content}
+              description={description}
+            />
+          )
+        )}
+      </div>
+    </>
   );
 };
 
--- src/backend/Newsitem.js
+++ src/backend/Newsitem.js
@@ -1,16 +1,40 @@
 import React from "react";
 
-const NewsItem = ({ title, description, url, urlToImage }) => {
+const NewsItem = ({ title, description, url, urlToImage, publishedAt , content }) => {
   return (
-    <div>
-      <img src={urlToImage} alt="" />
-      <h4>
-        <a href={url}>{title}</a>
-      </h4>
-      <p>{description}</p>
-      <p>read more</p>
-    </div>
+    <>
+      {/* article original */}
+{/*       <div>
+        <img src={urlToImage} alt="" />
+        <h4>
+          <a href={url}>{title}</a>
+        </h4>
+        <div>{publishedAt}</div>
+        <div>#ship #shipping #world #business</div>
+        <p>{description}</p>
+        <p>read more</p>
+      </div> */}
+
+      {/* article page 2 */}
+      <div>
+        <h1>
+            <a href={url}>{title}</a>
+        </h1>
+        <h1>{publishedAt}</h1>
+        <img src={urlToImage}/>
+        <p>{description}</p>
+        <p>{content}</p>
+
+      </div>
+    </>
   );
 };
 
 export default NewsItem;
+
+// img (urlToImage)
+// title (title)
+// date time (publishedAt) 12 Desember 2021
+// hashtag
+// description (description)
+// readmore
