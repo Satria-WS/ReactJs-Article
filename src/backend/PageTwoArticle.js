a0efd711c666b9dd6dec956bb46a3c0e32e530a8
Satria-WS <extencezone02@gmail.com>
2022-07-15 11:26:15 +0700

    Update PageTwoArticle.js

--- src/backend/PageTwoArticle.js
+++ src/backend/PageTwoArticle.js
@@ -6,7 +6,18 @@ import axios from "axios";
 const PageTwoArticle = () => {
     return (
       <>
-        <div className="BoxColoumnPageTwo" style={{top:"1269px",left:"120px"}}>hello </div>
+        <div
+          className="BoxColoumnPageTwo"
+          style={{ top: "1269px", left: "120px" }}
+        >
+          box1{" "}
+        </div>
+        <div
+          className="BoxColoumnPageTwo"
+          style={{ top: "478px", left: "769px" }}
+        >
+          box2
+        </div>
       </>
     );
 }
